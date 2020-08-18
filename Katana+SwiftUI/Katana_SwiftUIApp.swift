fa7c658f638b1ce08898df881c0974662c52bbbd
Michele Mola <michele.mola92@gmail.com>
2020-08-18 18:05:34 +0200

    Initial Commit

---
+++ Katana+SwiftUI/Katana_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Katana_SwiftUIApp.swift
+//  Katana+SwiftUI
+//
+//  Created by Michele Mola on 18/08/20.
+//
+
+import SwiftUI
+
+@main
+struct Katana_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
